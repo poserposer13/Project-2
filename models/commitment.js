0bd75f490ccffa914665a75f57bc7dfd6098fdf3
Sean Kempf <poserposer@me.com>
2020-11-14 12:31:06 -0700

    Created basic commitment model

---
+++ models/commitment.js
@@ -0,0 +1,40 @@
+module.exports = function (sequelize, DataTypes) {
+  const Commitment = sequelize.define("Commitment", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    payRate: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    accrualRate: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+  });
+
+  Commitment.associate = function (models) {
+    Commitment.belongsToMany(models.Department, {
+      foreignKey: {
+        allowNull: false,
+      },
+    });
+  };
+  Commitment.associate = function (models) {
+    Commitment.hasMany(models.Employee, {
+      onDelete: "cascade",
+    });
+  };
+
+  return Commitment;
+};
