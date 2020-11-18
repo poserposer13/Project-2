a1de4dd0e099d96281d2dae9005993c1dc1aece9
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-11-17 18:48:56 -0700

    Updated employee controller.

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 // Import our controllers
 const postRoutes = require("./postsController");
 const userRoutes = require("./usersController");
+const employeeRoutes = require("./employeeController");
 
 // Hook up to the router
 router.use("/posts", postRoutes);
 router.use("/users", userRoutes);
+router.use("/employee", employeeRoutes);
 
 // Export the router
 module.exports = router;
