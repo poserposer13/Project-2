7823887635ef15fab29104ecc4f2970fb3a17975
Sean Kempf <69017309+poserposer13@users.noreply.github.com>
2020-11-17 18:41:16 -0700

    Update index.js

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -5,13 +5,11 @@ const userRoutes = require("./usersController");
 const departmentRoutes = require("./departmentController");
 const payRoutes = require("./payController");
 
-
 // Hook up to the router
 router.use("/posts", postRoutes);
 router.use("/users", userRoutes);
 router.use("/departments", departmentRoutes);
 router.use("/pay", payRoutes);
-
 
 // Export the router
 module.exports = router;
