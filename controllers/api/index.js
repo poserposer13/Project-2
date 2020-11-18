3106de54d0ea30cb8e3e42b8f197ac90d464e4a5
Andrew Moses <andrewmosesdrive@gmail.com>
2020-11-17 18:29:08 -0700

    updated department controller for CRUD

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 // Import our controllers
 const postRoutes = require("./postsController");
 const userRoutes = require("./usersController");
+const departmentRoutes = require("./departmentController");
 
 // Hook up to the router
 router.use("/posts", postRoutes);
 router.use("/users", userRoutes);
+router.use("/departments", departmentRoutes);
 
 // Export the router
 module.exports = router;
