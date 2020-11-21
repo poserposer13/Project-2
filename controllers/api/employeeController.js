8ae28ebdca2c3b207e882f9457f78b0291f2bf6b
solaguirre <68126593+solaguirre@users.noreply.github.com>
2020-11-21 11:47:28 -0700

    Updated employee controller.

--- controllers/api/employeeController.js
+++ controllers/api/employeeController.js
@@ -1,40 +1,37 @@
 const db = require("../../models");
 const router = require("express").Router();
 
-router.get("/api/employee/", (req, res) => {
+router.get("/", (req, res) => {
   db.Employee.findAll({
     query: req.query,
-    include: [db.Commitment, db.Pay]
-      .then((dbEmployee) => res.json(dbEmployee))
-      .catch((err) => res.status(422).json(err)),
-  });
-});
-
-router.get("/:id", (req, res) => {
-  db.Employee.findByPk(req.params.id)
-    .then((dbEmployee) => res.json(dbEmployee))
-    .catch((err) => res.status(422).json(err));
-});
-
-/*** Employee - Update*/
-router.put("/:id", (req, res) => {
-  db.Employee.update(req.body, { where: { id: req.params.id } })
-    .then((dbEmployee) => res.json(dbEmployee))
-    .catch((err) => res.status(422).json(err));
-});
-
-router.post("/", (req, res) => {
-  db.Employee.create(req.body).then((dbEmployee) => {
+    include: [db.Commitment, db.Pay],
+  }).then((dbEmployee) => {
     res.json(dbEmployee);
   });
-});
 
-/*** Employee - Delete*/
-router.delete("/:id", (req, res) => {
-  db.Employee.destroy({
-    where: { id: req.params.id },
-  })
-    .then((dbEmployee) => res.json(dbEmployee))
-    .catch((err) => res.status(422).json(err));
-});
-module.exports = router;
+  router.get("/:id", (req, res) => {
+    db.Employee.findByPk(req.params.id)
+      .then((dbEmployee) => res.json(dbEmployee))
+      .catch((err) => res.status(422).json(err));
+  });
+
+  // create
+  router.post("/", (req, res) => {
+    db.Employee.create(req.body).then((dbEmployee) => res.json(dbEmployee));
+    });
+  });
+
+  /*** Employee - Update*/
+  router.put("/:id", (req, res) => {
+    db.Employee.update(req.body, {
+      where: { id: req.params.id },
+    }).then((dbEmployee) => res.json(dbEmployee));
+  });
+
+  /*** Employee - Delete*/
+  router.delete("/:id", (req, res) => {
+    db.Employee.destroy({
+      where: { id: req.params.id },
+    }).then((dbEmployee) => res.json(dbEmployee));
+  });
+  module.exports = router;
